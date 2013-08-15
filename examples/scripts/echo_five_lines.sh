85b05693b9439e7df8b116c7de8dbba833ce0f6a
David Pärsson <david@parsson.se>
2013-08-15 18:14:54 +0200

    Added prototype version

---
+++ examples/scripts/echo_five_lines.sh
@@ -0,0 +1,11 @@
+sleep 0.4
+echo '1: First line says hi!'
+sleep 0.4
+echo '2: Second line!'
+sleep 0.4
+echo '3: Third line says hi!'
+sleep 0.4
+echo '4: Fourth line!'
+sleep 0.4
+echo '5: Fifth line says hi!'
+sleep 1
---
+++ src/main/groovy/se/dreampark/ProgressLoggerOutputStream.groovy
@@ -0,0 +1,78 @@
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.LogLevel
+import org.gradle.logging.ProgressLogger
+import org.gradle.logging.ProgressLoggerFactory
+
+class ProgressLoggerOutputStream extends OutputStream {
+    Map<String, Closure> patterns = new HashMap<String, Closure>()
+    ProgressLogger progressLogger
+    Logger logger
+    StringBuilder line = new StringBuilder()
+    Boolean started = false
+    LogLevel logLevel = LogLevel.INFO
+    int NEW_LINE_BYTE = (int) '\n'
+
+    ProgressLoggerOutputStream(ProgressLogger progressLogger, Logger logger) {
+        this.progressLogger = progressLogger
+        this.logger = logger
+    }
+
+    public void write(int b) {
+        if (b != NEW_LINE_BYTE) {
+            line.append((char) b)
+        } else {
+            logMatchingLine();
+        }
+    }
+
+    public void close() {
+        if (started) {
+            progressLogger.completed()
+            started = false
+        }
+    }
+
+    public void addPattern(String findPattern) {
+        addPattern(findPattern, null)
+    }
+
+    public void addPattern(String findPattern, Closure replace) {
+        patterns.put(findPattern, replace)
+    }
+
+    public void setLogLevel(LogLevel logLevel) {
+        this.logLevel = logLevel
+    }
+
+    private void logMatchingLine() {
+        def lastLine = consumeLastLine()
+        logger.log(logLevel, lastLine)
+        patterns.each { findPattern, replace ->
+            if (lastLine.matches(findPattern)) {
+                if (replace != null) {
+                    log(lastLine.replaceFirst(findPattern, replace))
+                } else {
+                    log(lastLine)
+                }
+            }
+        }
+    }
+
+    private String consumeLastLine() {
+        String lastLine = line.toString()
+        line.setLength(0)
+        lastLine
+    }
+
+    private void log(String line) {
+        def logger = progressLogger
+        if (started) {
+            logger.progress(line)
+        } else {
+            logger.setDescription("-")
+            logger.started(line)
+            started = true
+        }
+    }
+
+}
