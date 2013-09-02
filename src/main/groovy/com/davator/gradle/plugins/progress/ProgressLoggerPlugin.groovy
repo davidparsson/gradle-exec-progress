c2ac1adfd9ce80ba9720163516b73b4647d49a74
David Pärsson <david@parsson.se>
2013-09-02 16:09:06 +0200

    Only applying progress to Exec tasks

--- src/main/groovy/com/davator/gradle/plugins/progress/ProgressLoggerPlugin.groovy
+++ src/main/groovy/com/davator/gradle/plugins/progress/ProgressLoggerPlugin.groovy
@@ -2,16 +2,17 @@ package com.davator.gradle.plugins.progress
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Exec
 
 class ProgressLoggerPlugin implements Plugin<Project> {
 
     void apply(Project project) {
-        project.ext.progress = { Closure closure ->
+        Exec.metaClass.progress = { Closure closure ->
             def stream = new ProgressLoggerOutputStream(project)
             def configuration = new ProgressLoggerStreamConfiguration(stream)
             closure.delegate = configuration
             closure()
-            closure.standardOutput = stream
+            closure.owner.standardOutput = stream
         }
     }
 
